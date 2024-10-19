e7a561c0c1d969b2581bd9a12013b56263f9d446
Komal Khandelwal <kkomal1911@gmail.com>
2024-10-18 23:09:32 -0700

    react1

---
+++ app.js
@@ -0,0 +1,8 @@
+<body>
+<script>
+    const heading=document.createElement("h1");
+    heading.innerHTML("Hello World!! from javascript");
+    const divElement=document.getElementById("root");
+    divElement.appendChild(heading);
+</script>
+</body> 
